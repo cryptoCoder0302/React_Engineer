055ae7c45c522bc399c7b486104efc1308a9fc8f
Acetobi0302 <acetobi@outlook.com>
2015-08-24 22:48:41 +1000

    Updating build

--- test/Select-test.js
+++ test/Select-test.js
@@ -947,6 +947,124 @@ describe('Select', function() {
 		});
 	});
 
+	describe('with styled options', function () {
+
+		beforeEach(function () {
+
+			options = [
+				{ value: 'one', label: 'One', className: 'extra-one', title: 'Eins' },
+				{ value: 'two', label: 'Two', className: 'extra-two', title: 'Zwei' },
+				{ value: 'three', label: 'Three', style: { fontSize: 25 } }
+			];
+
+			wrapper = createControlWithWrapper({
+				options: options
+			});
+		});
+
+		it('uses the given className for an option', function () {
+
+			clickArrowToOpen();
+			expect(React.findDOMNode(instance).querySelectorAll('.Select-option')[0], 'to have attributes',
+				{
+					class: 'extra-one'
+				});
+		});
+
+		it('uses the given style for an option', function () {
+
+			clickArrowToOpen();
+			expect(React.findDOMNode(instance).querySelectorAll('.Select-option')[2], 'to have attributes',
+				{
+					style: { 'font-size': '25px' }
+				});
+		});
+
+		it('uses the given title for an option', function () {
+
+			clickArrowToOpen();
+			expect(React.findDOMNode(instance).querySelectorAll('.Select-option')[1], 'to have attributes',
+				{
+					title: 'Zwei'
+				});
+		});
+
+		it('uses the given className for a single selection', function () {
+
+			typeSearchText('tw');
+			pressEnterToAccept();
+			expect(React.findDOMNode(instance), 'queried for first', DISPLAYED_SELECTION_SELECTOR,
+				'to have attributes', {
+					class: 'extra-two'
+				});
+		});
+
+		it('uses the given style for a single selection', function () {
+
+			typeSearchText('th');
+			pressEnterToAccept();
+			expect(React.findDOMNode(instance), 'queried for first', DISPLAYED_SELECTION_SELECTOR,
+				'to have attributes', {
+					style: {
+						'font-size': '25px'
+					}
+				});
+		});
+
+		it('uses the given title for a single selection', function () {
+
+			typeSearchText('tw');
+			pressEnterToAccept();
+			expect(React.findDOMNode(instance), 'queried for first', DISPLAYED_SELECTION_SELECTOR,
+				'to have attributes', {
+					title: 'Zwei'
+				});
+		});
+
+		describe('with multi', function () {
+
+			beforeEach(function () {
+
+				wrapper.setPropsForChild({ multi: true });
+			});
+
+
+			it('uses the given className for a selected value', function () {
+
+				typeSearchText('tw');
+				pressEnterToAccept();
+				expect(React.findDOMNode(instance), 'queried for first', '.Select-item',
+					'to have attributes', {
+						class: 'extra-two'
+					});
+			});
+
+			it('uses the given style for a selected value', function () {
+
+				typeSearchText('th');
+				pressEnterToAccept();
+				expect(React.findDOMNode(instance), 'queried for first', '.Select-item',
+					'to have attributes', {
+						style: {
+							'font-size': '25px'
+						}
+					});
+			});
+
+			it('uses the given title for a selected value', function () {
+
+				typeSearchText('tw');
+				pressEnterToAccept();
+				expect(React.findDOMNode(instance), 'queried for first', '.Select-item',
+					'to have attributes', {
+						title: 'Zwei'
+					});
+			});
+
+		});
+
+	});
+
 	describe('with allowCreate=true', function () {
 
 		beforeEach(function () {
@@ -2429,6 +2547,69 @@ describe('Select', function() {
 			});
 		});
 
+		describe('searchingText', function () {
+
+			var asyncOptions;
+			var asyncOptionsCallback;
+
+			beforeEach(function () {
+
+				asyncOptions = sinon.spy();
+
+				instance = createControl({
+					asyncOptions: asyncOptions,
+					autoload: false,
+					searchingText: 'Testing async loading...',
+					noResultsText: 'Testing No results found',
+					searchPromptText: 'Testing enter search query'
+				});
+			});
+
+			it('uses the searchingText whilst the asyncOptions are loading', function () {
+
+				clickArrowToOpen();
+				expect(asyncOptions, 'was not called');
+				typeSearchText('abc');
+				expect(asyncOptions, 'was called');
+				expect(React.findDOMNode(instance), 'to contain elements matching', '.Select-loading');
+				expect(React.findDOMNode(instance), 'queried for first', '.Select-searching',
+					'to have text', 'Testing async loading...');
+			});
+
+			it('clears the searchingText when results arrive', function () {
+
+				clickArrowToOpen();
+				typeSearchText('abc');
+
+				expect(React.findDOMNode(instance), 'queried for first', '.Select-searching',
+					'to have text', 'Testing async loading...');
+
+				asyncOptions.args[0][1](null, {
+					options: [{ value: 'abc', label: 'Abc' }]
+				});
+
+				expect(React.findDOMNode(instance), 'to contain no elements matching', '.Select-noresults');
+			});
+
+			it('switches the searchingText to noResultsText when options arrive, but empty', function () {
+
+				clickArrowToOpen();
+				typeSearchText('abc');
+
+				expect(React.findDOMNode(instance), 'queried for first', '.Select-searching',
+					'to have text', 'Testing async loading...');
+				expect(React.findDOMNode(instance), 'to contain no elements matching', '.Select-noresults');
+
+				asyncOptions.args[0][1](null, {
+					options: []
+				});
+
+				expect(React.findDOMNode(instance), 'queried for first', '.Select-noresults',
+					'to have text', 'Testing No results found');
+				expect(React.findDOMNode(instance), 'to contain no elements matching', '.Select-searching');
+			});
+		});
+
 		describe('searchPromptText', function () {
 			var asyncOptions;
 
@@ -2448,7 +2629,7 @@ describe('Select', function() {
 				var selectArrow = React.findDOMNode(instance).querySelector('.Select-arrow');
 				TestUtils.Simulate.mouseDown(selectArrow);
 
-				expect(React.findDOMNode(instance), 'queried for', '.Select-noresults',
+				expect(React.findDOMNode(instance), 'queried for', '.Select-search-prompt',
 					'to have items satisfying',
 					'to have text', 'Unit test prompt text');
 			});
@@ -2465,6 +2646,7 @@ describe('Select', function() {
 				typeSearchText('abc');
 				expect(asyncOptions, 'was called once');
 
+				expect(React.findDOMNode(instance), 'to contain no elements matching', '.Select-prompt');
 				expect(React.findDOMNode(instance), 'to contain no elements matching', '.Select-noresults');
 			});
 		});
