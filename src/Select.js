fcb3db2d751710d1f4fc287a83aeee5ba1d605e6
Acetobi0302 <acetobi@outlook.com>
2015-09-27 13:41:56 +0200

    Get ready for React 0.14

--- src/Select.js
+++ src/Select.js
@@ -3,6 +3,7 @@
 /* eslint react/jsx-sort-prop-types: 0, react/sort-comp: 0, react/prop-types: 0 */
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Input = require('react-input-autosize');
 var classes = require('classnames');
 var Value = require('./Value');
@@ -32,7 +33,6 @@ var Select = React.createClass({
 		filterOptions: React.PropTypes.func,       // method to filter the options array: function([options], filterString, [values])
 		ignoreCase: React.PropTypes.bool,          // whether to perform case-insensitive filtering
 		inputProps: React.PropTypes.object,        // custom attributes for the Input (in the Select-control) e.g: {'data-foo': 'bar'}
-		isLoading: React.PropTypes.bool,           // whether the Select is loading externally or not (such as options being loaded)
 		matchPos: React.PropTypes.string,          // (any|start) match the start or entire string when filtering
 		matchProp: React.PropTypes.string,         // (any|label|value) which option property to filter on
 		multi: React.PropTypes.bool,               // multi-value input
@@ -73,7 +73,6 @@ var Select = React.createClass({
 			disabled: false,
 			ignoreCase: true,
 			inputProps: {},
-			isLoading: false,
 			matchPos: 'any',
 			matchProp: 'any',
 			name: undefined,
@@ -119,8 +118,8 @@ var Select = React.createClass({
 			if (!this.state.isOpen) {
 				return;
 			}
-			var menuElem = React.findDOMNode(this.refs.selectMenuContainer);
-			var controlElem = React.findDOMNode(this.refs.control);
+			var menuElem = ReactDOM.findDOMNode(this.refs.selectMenuContainer);
+			var controlElem = ReactDOM.findDOMNode(this.refs.control);
 
 			var eventOccuredOutsideMenu = this.clickedOutsideElement(menuElem, event);
 			var eventOccuredOutsideControl = this.clickedOutsideElement(controlElem, event);
@@ -197,8 +196,8 @@ var Select = React.createClass({
 		}
 		if (this._focusedOptionReveal) {
 			if (this.refs.focused && this.refs.menu) {
-				var focusedDOM = React.findDOMNode(this.refs.focused);
-				var menuDOM = React.findDOMNode(this.refs.menu);
+				var focusedDOM = ReactDOM.findDOMNode(this.refs.focused);
+				var menuDOM = ReactDOM.findDOMNode(this.refs.menu);
 				var focusedRect = focusedDOM.getBoundingClientRect();
 				var menuRect = menuDOM.getBoundingClientRect();
 
@@ -347,7 +346,7 @@ var Select = React.createClass({
 
 	getInputNode: function () {
 		var input = this.refs.input;
-		return this.props.searchable ? input : React.findDOMNode(input);
+		return this.props.searchable ? input : ReactDOM.findDOMNode(input);
 	},
 
 	fireChangeEvent: function(newState) {
@@ -730,7 +729,7 @@ var Select = React.createClass({
 			return ops;
 		} else {
 			var noResultsText, promptClass;
-			if (this.isLoading()) {
+			if (this.state.isLoading) {
 				promptClass = 'Select-searching';
 				noResultsText = this.props.searchingText;
 			} else if (this.state.inputValue || !this.props.asyncOptions) {
@@ -755,17 +754,13 @@ var Select = React.createClass({
 		}
 	},
 
-	isLoading: function() {
-		return this.props.isLoading || this.state.isLoading;
-	},
-
 	render: function() {
 		var selectClass = classes('Select', this.props.className, {
 			'is-multi': this.props.multi,
 			'is-searchable': this.props.searchable,
 			'is-open': this.state.isOpen,
 			'is-focused': this.state.isFocused,
-			'is-loading': this.isLoading(),
+			'is-loading': this.state.isLoading,
 			'is-disabled': this.props.disabled,
 			'has-value': this.state.value
 		});
@@ -805,7 +800,7 @@ var Select = React.createClass({
 			}
 		}
 
-		var loading = this.isLoading() ? <span className="Select-loading" aria-hidden="true" /> : null;
+		var loading = this.state.isLoading ? <span className="Select-loading" aria-hidden="true" /> : null;
 		var clear = this.props.clearable && this.state.value && !this.props.disabled ? <span className="Select-clear" title={this.props.multi ? this.props.clearAllText : this.props.clearValueText} aria-label={this.props.multi ? this.props.clearAllText : this.props.clearValueText} onMouseDown={this.clearValue} onTouchEnd={this.clearValue} onClick={this.clearValue} dangerouslySetInnerHTML={{ __html: '&times;' }} /> : null;
 
 		var menu;
