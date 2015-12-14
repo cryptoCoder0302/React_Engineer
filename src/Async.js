83b32c9036bf543332d1b803aa13af9f6b73584d
Acetobi0302 <acetobi@outlook.com>
2015-12-14 18:58:49 +0800

    Allow customization of menu option background color

--- src/Async.js
+++ src/Async.js
@@ -46,10 +46,7 @@ const Async = React.createClass({
 		loadingPlaceholder: React.PropTypes.string,     // replaces the placeholder while options are loading
 		minimumInput: React.PropTypes.number,           // the minimum number of characters that trigger loadOptions
 		noResultsText: React.PropTypes.string,          // placeholder displayed when there are no matching search results (shared with Select)
-		placeholder: React.PropTypes.oneOfType([        // field placeholder, displayed when there's no value (shared with Select)
-			React.PropTypes.string,
-			React.PropTypes.node
-		]),
+		placeholder: React.PropTypes.string,            // field placeholder, displayed when there's no value (shared with Select)
 		searchingText: React.PropTypes.string,          // message to display while options are loading
 		searchPromptText: React.PropTypes.string,       // label to prompt for search input
 	},
