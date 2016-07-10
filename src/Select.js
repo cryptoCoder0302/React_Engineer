1041becb8334bed954c98b54e6329c48c83ecbc7
Acetobi0302 <acetobi@outlook.com>
2016-07-10 23:47:04 +1000

    Updating react-input-autosize, fixes #1073

--- src/Select.js
+++ src/Select.js
@@ -169,7 +169,7 @@ const Select = React.createClass({
 	},
 
 	componentWillReceiveProps(nextProps) {
-		const valueArray = this.getValueArray(nextProps.value, nextProps);
+		const valueArray = this.getValueArray(nextProps.value);
 
 		if (nextProps.required) {
 			this.setState({
@@ -477,35 +477,22 @@ const Select = React.createClass({
 		return op[this.props.labelKey];
 	},
 
-	/**
-	 * Turns a value into an array from the given options
-	 * @param	{String|Number|Array}	value		- the value of the select input
-	 * @param	{Object}		nextProps	- optionally specify the nextProps so the returned array uses the latest configuration
-	 * @returns	{Array}	the value of the select represented in an array
-	 */
-	getValueArray (value, nextProps) {
-		/** support optionally passing in the `nextProps` so `componentWillReceiveProps` updates will function as expected */
-		const props = typeof nextProps === 'object' ? nextProps : this.props;
-		if (props.multi) {
-			if (typeof value === 'string') value = value.split(props.delimiter);
+	getValueArray (value) {
+		if (this.props.multi) {
+			if (typeof value === 'string') value = value.split(this.props.delimiter);
 			if (!Array.isArray(value)) {
 				if (value === null || value === undefined) return [];
 				value = [value];
 			}
-			return value.map(value => this.expandValue(value, props)).filter(i => i);
+			return value.map(this.expandValue).filter(i => i);
 		}
-		var expandedValue = this.expandValue(value, props);
+		var expandedValue = this.expandValue(value);
 		return expandedValue ? [expandedValue] : [];
 	},
 
-	/**
-	 * Retrieve a value from the given options and valueKey
-	 * @param	{String|Number|Array}	value	- the selected value(s)
-	 * @param	{Object}		props	- the Select component's props (or nextProps)
-	 */
-	expandValue (value, props) {
+	expandValue (value) {
 		if (typeof value !== 'string' && typeof value !== 'number') return value;
-		let { options, valueKey } = props;
+		let { options, valueKey } = this.props;
 		if (!options) return;
 		for (var i = 0; i < options.length; i++) {
 			if (options[i][valueKey] === value) return options[i];
