92012b6e70ea8ed2d88ac6fbc48fed89f516820e
Acetobi0302 <acetobi@outlook.com>
2017-07-30 17:10:02 -0400

    Add tests for removeSelected

--- src/Select.js
+++ src/Select.js
@@ -520,7 +520,12 @@ class Select extends React.Component {
 				inputValue: this.handleInputValueChange(''),
 				focusedIndex: null
 			}, () => {
-				this.addValue(value);
+				var valueArray = this.getValueArray(this.props.value);
+				if (valueArray.some(i => i[this.props.valueKey] === value[this.props.valueKey])) {
+					this.removeValue(value);
+				} else {
+					this.addValue(value);
+				}
 			});
 		} else {
 			this.setState({
@@ -556,7 +561,7 @@ class Select extends React.Component {
 
 	removeValue (value) {
 		var valueArray = this.getValueArray(this.props.value);
-		this.setValue(valueArray.filter(i => i !== value));
+		this.setValue(valueArray.filter(i => i[this.props.valueKey] !== value[this.props.valueKey]));
 		this.focus();
 	}
 
@@ -962,7 +967,7 @@ class Select extends React.Component {
 
 		return (
 			<div ref={ref => this.menuContainer = ref} className="Select-menu-outer" style={this.props.menuContainerStyle}>
-				<div ref={ref => this.menu = ref} role="listbox" tabIndex={-1} className="Select-menu" id={this._instancePrefix + '-list'}
+				<div ref={ref => this.menu = ref} role="listbox" className="Select-menu" id={this._instancePrefix + '-list'}
 						 style={this.props.menuStyle}
 						 onScroll={this.handleMenuScroll}
 						 onMouseDown={this.handleMouseDownOnMenu}>
@@ -974,7 +979,7 @@ class Select extends React.Component {
 
 	render () {
 		let valueArray = this.getValueArray(this.props.value);
-		let options = this._visibleOptions = this.filterOptions(this.props.multi ? this.getValueArray(this.props.value) : null);
+		let options = this._visibleOptions = this.filterOptions(this.props.multi && this.props.removeSelected ? valueArray : null);
 		let isOpen = this.state.isOpen;
 		if (this.props.multi && !options.length && valueArray.length && !this.state.inputValue) isOpen = false;
 		const focusedOptionIndex = this.getFocusableOptionIndex(valueArray[0]);
@@ -1035,7 +1040,7 @@ class Select extends React.Component {
 					{this.renderClear()}
 					{this.renderArrow()}
 				</div>
-				{isOpen ? this.renderOuter(options, !this.props.multi ? valueArray : null, focusedOption) : null}
+				{isOpen ? this.renderOuter(options, valueArray, focusedOption) : null}
 			</div>
 		);
 	}
@@ -1099,6 +1104,7 @@ Select.propTypes = {
     options: PropTypes.array,             // array of options
     pageSize: PropTypes.number,           // number of entries to page when using page up/down keys
     placeholder: stringOrNode,            // field placeholder, displayed when there's no value
+    removeSelected: PropTypes.bool,       // whether the selected option is removed from the dropdown on multi selects
     required: PropTypes.bool,             // applies HTML5 required attribute when needed
     resetValue: PropTypes.any,            // value to use when you clear the control
     scrollMenuIntoView: PropTypes.bool,   // boolean to enable the viewport to shift so that the full menu fully visible when engaged
@@ -1146,6 +1152,7 @@ Select.defaultProps = {
     optionComponent: Option,
     pageSize: 5,
     placeholder: 'Select...',
+    removeSelected: true,
     required: false,
     scrollMenuIntoView: true,
     searchable: true,
