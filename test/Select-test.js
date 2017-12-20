362e0c8628ec2a4bff3bf4fc0547fcc336c76cb0
Acetobi0302 <acetobi@outlook.com>
2017-12-20 08:51:07 +0200

    arrowRenderer changed to arrow function

--- test/Select-test.js
+++ test/Select-test.js
@@ -2990,6 +2990,7 @@ describe('Select', () => {
 					instance = createControl({
 						searchable: false,
 						inputProps: {
+							inputClassName: 'extra-input-class',
 							className: 'extra-class-name',
 							id: 'search-input-id'
 						},
