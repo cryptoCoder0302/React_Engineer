e476f484dc26ba70ae2f1fef49e339e347d0172c
Acetobi0302 <acetobi@outlook.com>
2018-03-29 11:35:20 +0300

    fix table layout in Readme

--- docs/PropTypes/components/NoOptionsMessage.js
+++
@@ -1,4 +0,0 @@
-import { Component } from 'react';
-import { type NoticeProps } from '../../../src/components/Menu';
-
-export default class NoOptionsMessage extends Component<NoticeProps> {}
--- src/internal/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-export { default as DummyInput } from './DummyInput';
-export { default as NodeResolver } from './NodeResolver';
-export { default as ScrollCaptor } from './ScrollCaptor';
