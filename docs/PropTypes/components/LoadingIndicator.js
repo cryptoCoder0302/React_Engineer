2a9036bbfcbc5e40d972619f95469b8ca32e835b
Acetobi0302 <acetobi@outlook.com>
2018-04-20 18:11:36 +1000

    Adding notice about v2.0.0 beta

--- docs/PropTypes/components/LoadingIndicator.js
+++
@@ -1,4 +0,0 @@
-import { Component } from 'react';
-import { type LoadingIconProps } from '../../../src/components/indicators';
-
-export default class LoadingIndicator extends Component<LoadingIconProps> {}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-// @flow
+import Select from './Select';
+import Async from './Async';
+import AsyncCreatable from './AsyncCreatable';
+import Creatable from './Creatable';
+import Value from './Value';
+import Option from './Option';
 
-import SelectBase from './Select';
-import manageState from './stateManager';
+import defaultMenuRenderer from './utils/defaultMenuRenderer';
+import defaultArrowRenderer from './utils/defaultArrowRenderer';
+import defaultClearRenderer from './utils/defaultClearRenderer';
+import defaultFilterOptions from './utils/defaultFilterOptions';
 
-export default manageState(SelectBase);
-export { SelectBase };
-export { createFilter } from './filters';
-export { components } from './components/index';
-export { mergeStyles } from './styles';
+Select.Async = Async;
+Select.AsyncCreatable = AsyncCreatable;
+Select.Creatable = Creatable;
+Select.Value = Value;
+Select.Option = Option;
+
+export default Select;
+export {
+	Async,
+	AsyncCreatable,
+	Creatable,
+	Value,
+	Option,
+	defaultMenuRenderer,
+	defaultArrowRenderer,
+	defaultClearRenderer,
+	defaultFilterOptions
+};
