268838dce4686cc36ce709d3c88c1e996fa27c93
Acetobi0302 <acetobi@outlook.com>
2018-04-20 18:17:09 +1000

    Updating yarn.lock

---
+++ docs/data.js
@@ -0,0 +1,41 @@
+export const colourOptions = [
+  { value: 'ocean', label: 'Ocean', color: '#00B8D9' },
+  { value: 'blue', label: 'Blue', color: '#0052CC', disabled: true },
+  { value: 'purple', label: 'Purple', color: '#5243AA' },
+  { value: 'red', label: 'Red', color: '#FF5630' },
+  { value: 'orange', label: 'Orange', color: '#FF8B00' },
+  { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+  { value: 'green', label: 'Green', color: '#36B37E' },
+  { value: 'forest', label: 'Forest', color: '#00875A' },
+  { value: 'slate', label: 'Slate', color: '#253858' },
+  { value: 'silver', label: 'Silver', color: '#666666' },
+];
+
+export const flavourOptions = [
+  { value: 'vanilla', label: 'Vanilla', rating: 'safe' },
+  { value: 'chocolate', label: 'Chocolate', rating: 'good' },
+  { value: 'strawberry', label: 'Strawberry', rating: 'wild' },
+  { value: 'salted-caramel', label: 'Salted Caramel', rating: 'crazy' },
+];
+
+export const optionLength = [
+  { value: 1, label: 'general' },
+  { value: 2, label: 'Evil is the moment when I lack the strength to be true to the Good that compels me.' },
+  { value: 3, label: "It is now an easy matter to spell out the ethic of a truth: 'Do all that you can to persevere in that which exceeds your perseverance. Persevere in the interruption. Seize in your being that which has seized and broken you." }
+];
+
+// let bigOptions = [];
+// for (let i = 0; i < 10000; i++) {
+// 	bigOptions = bigOptions.concat(colourOptions);
+// }
+
+export const groupedOptions = [
+  {
+    label: 'Colours',
+    options: colourOptions,
+  },
+  {
+    label: 'Flavours',
+    options: flavourOptions,
+  },
+];
