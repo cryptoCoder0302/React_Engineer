1a7cc96a5560246f56226af02d78486cf7c9a0c5
Acetobi0302 <acetobi@outlook.com>
2018-04-23 22:45:52 +1000

    Removing (now) unnecessary white space from flex styles

--- src/index.umd.js
+++ src/index.umd.js
@@ -6,9 +6,9 @@
 import SelectBase from './Select';
 import manageState from './stateManager';
 
-import { createFilter } from './filters';
-import { components } from './components/index';
-import { mergeStyles } from './styles';
+export { createFilter } from './filters';
+export { components } from './components/index';
+export { mergeStyles } from './styles';
 
 const Select = manageState(SelectBase);
 Select.SelectBase = SelectBase;
