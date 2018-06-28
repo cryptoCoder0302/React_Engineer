d795fdebea7dc032df9d096704d5a9d5bb86b710
Acetobi0302 <acetobi@outlook.com>
2018-06-28 17:28:01 +1000

    fix mobile scroll blocking

--- src/animated/Input.js
+++ src/animated/Input.js
@@ -1,27 +1,26 @@
 // @flow
 
-import React, { type ComponentType } from 'react';
-import { type InputProps } from '../components/Input';
+import React from 'react';
+import { components } from '../components';
+
 import { type BaseTransition } from './transitions';
 import { type PropsWithInnerRef } from '../types';
 
-type AnimatedInputProps = BaseTransition & PropsWithInnerRef;
+type InputProps = BaseTransition & PropsWithInnerRef;
 
 // strip transition props off before spreading onto select component
 // note we need to be explicit about innerRef for flow
-const AnimatedInput = (WrappedComponent: ComponentType<InputProps>) => {
-  return ({
-    in: inProp,
-    onExited,
-    appear,
-    enter,
-    exit,
-    innerRef,
-    ...props
-  }: AnimatedInputProps) => (
-    // $FlowFixMe
-    <WrappedComponent innerRef={innerRef} {...props}/>
-  );
+const AnimatedInput = ({
+  in: inProp,
+  onExited,
+  appear,
+  enter,
+  exit,
+  innerRef,
+  ...props
+}: InputProps) => {
+  // $FlowFixMe
+  return <components.Input innerRef={innerRef} {...props} />;
 };
 
 export default AnimatedInput;
