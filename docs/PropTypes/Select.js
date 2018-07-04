09ed06ef8bcf7929ed0d5e3dac69d91468579bec
Acetobi0302 <acetobi@outlook.com>
2018-07-04 09:23:17 +0700

    Fix #1620 (#2704)

--- docs/PropTypes/Select.js
+++
@@ -1,7 +0,0 @@
-import { Component } from 'react';
-
-import { type Props, defaultProps } from '../../src/Select';
-
-export default class Select extends Component<Props> {
-  defaultProps = defaultProps
-}
