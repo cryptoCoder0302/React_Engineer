9ba11f0782727d936cf681a5196c04e7b4b26833
Acetobi0302 <acetobi@outlook.com>
2018-07-10 08:24:59 +0300

    Tidy up rollup config (#2781)

--- docs/PropTypes/components/SingleValue.js
+++
@@ -1,4 +0,0 @@
-import { Component } from 'react';
-import { type SingleValueProps } from '../../../src/components/SingleValue';
-
-export default class SingleValue extends Component<SingleValueProps> {}
