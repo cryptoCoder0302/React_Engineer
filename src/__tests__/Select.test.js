5b1d39396d145700654f60ca76c5567eb29a3d5b
Acetobi0302 <acetobi@outlook.com>
2019-03-30 18:28:23 +1300

    Add labels to fields to make inspection easier

--- src/__tests__/Select.test.js
+++ src/__tests__/Select.test.js
@@ -1835,30 +1835,15 @@ cases(
   },
   {
     'single select > should display default placeholder "Select..."': {},
-    'single select > should display provided string placeholder': {
+    'single select > should display provided placeholder': {
       props: {
-        ...BASIC_PROPS,
         placeholder: 'single Select...',
       },
       expectPlaceholder: 'single Select...',
     },
-    'single select > should display provided node placeholder': {
-      props: {
-        ...BASIC_PROPS,
-        placeholder: <span>single Select...</span>,
-      },
-      expectPlaceholder: 'single Select...',
-    },
-    'multi select > should display default placeholder "Select..."': {
-      props: {
-        ...BASIC_PROPS,
-        isMulti: true
-      }
-    },
+    'multi select > should display default placeholder "Select..."': {},
     'multi select > should display provided placeholder': {
       props: {
-        ...BASIC_PROPS,
-        isMulti: true,
         placeholder: 'multi Select...',
       },
       expectPlaceholder: 'multi Select...',
