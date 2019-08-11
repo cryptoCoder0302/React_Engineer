edfbc2f5a9a571f5f4dd69a993c248f77d52806c
Acetobi0302 <acetobi@outlook.com>
2019-08-12 01:08:08 +0800

    Marked deprecated react lifecycles as UNSAFE_*

--- packages/react-select/src/Async.js
+++ packages/react-select/src/Async.js
@@ -23,7 +23,6 @@ export type AsyncProps = {
   cacheOptions: any,
   onInputChange: (string, InputActionMeta) => void,
   inputValue?: string,
-  isLoading: boolean
 };
 
 export type Props = SelectProps & AsyncProps;
@@ -32,7 +31,6 @@ export const defaultProps = {
   cacheOptions: false,
   defaultOptions: false,
   filterOption: null,
-  isLoading: false
 };
 
 type State = {
@@ -78,7 +76,7 @@ export const makeAsyncSelect = <C: {}>(
         });
       }
     }
-    componentWillReceiveProps(nextProps: C & AsyncProps) {
+    UNSAFE_componentWillReceiveProps(nextProps: C & AsyncProps) {
       // if the cacheOptions prop changes, clear the cache
       if (nextProps.cacheOptions !== this.props.cacheOptions) {
         this.optionsCache = {};
@@ -160,7 +158,7 @@ export const makeAsyncSelect = <C: {}>(
       return inputValue;
     };
     render() {
-      const { loadOptions, isLoading: isLoadingProp, ...props } = this.props;
+      const { loadOptions, ...props } = this.props;
       const {
         defaultOptions,
         inputValue,
@@ -181,7 +179,7 @@ export const makeAsyncSelect = <C: {}>(
             this.select = ref;
           }}
           options={options}
-          isLoading={isLoading || isLoadingProp}
+          isLoading={isLoading}
           onInputChange={this.handleInputChange}
         />
       );
