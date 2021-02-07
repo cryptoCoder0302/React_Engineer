3311f45d6ae329354ae2756674626452596cf8c2
Acetobi0302 <acetobi@outlook.com>
2021-02-06 18:12:45 -0800

    Announce menu option is selected if focused, add tests. Resolves issue #4343

--- cypress/integration/single-select.spec.js
+++ cypress/integration/single-select.spec.js
@@ -171,16 +171,6 @@ describe('Single Select', () => {
           .get(selector.focusedOption)
           .should('exist');
       });
-
-      it(`Should focus next option on down arrow key press after filtering: ${viewport}`, () => {
-        cy.get(selector.singleGroupedSelect)
-          .click()
-          .find('input')
-          .type('o', { force: true })
-          .type('{downarrow}', { force: true })
-          .get(selector.focusedOption)
-          .should('exist');
-      });
     });
 
     context(`Clearable in view: ${viewport}`, () => {
