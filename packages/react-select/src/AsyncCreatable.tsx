6627f6929d8062daf95723255c78bbf6293e2a45
Acetobi0302 <acetobi@outlook.com>
2021-02-21 00:24:20 -0800

    Add aria-live prop to customize attribute on LiveRegion

--- packages/react-select/src/AsyncCreatable.tsx
+++
@@ -1,9 +0,0 @@
-import { makeAsyncSelect } from './Async';
-import { makeCreatableSelect } from './Creatable';
-import manageState from './stateManager';
-import Select from './Select';
-
-const SelectCreatable = makeCreatableSelect(Select);
-const SelectCreatableState = manageState(SelectCreatable);
-
-export default makeAsyncSelect(SelectCreatableState);
