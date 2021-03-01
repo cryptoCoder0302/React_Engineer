1a154a0263fd0b3e740cb96bc2bb98284b216372
Acetobi0302 <acetobi@outlook.com>
2021-03-01 14:53:13 -0600

    Refactor formatting functions to have one object prop, add prop comments

--- docs/isArray.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Alternative to Array.isArray that correctly narrows the type for readonly arrays.
- */
-export default function isArray<T>(arg: unknown): arg is readonly T[] {
-  return Array.isArray(arg);
-}
